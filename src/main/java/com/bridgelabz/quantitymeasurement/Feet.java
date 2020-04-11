cd930c41aaef6869ae416ef16eae20612e683bf2
Devki1 <gdev3195@gmail.com>
2020-04-11 14:52:10 +0530

    ADDED:Feet class and override equal method

---
+++ src/main/java/com/bridgelabz/quantitymeasurement/Feet.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.quantitymeasurement;
+
+public class Feet {
+    public double length;
+
+    public Feet(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.length, length) == 0;
+    }
+}
