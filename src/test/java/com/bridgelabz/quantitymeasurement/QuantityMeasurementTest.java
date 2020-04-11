c6b02f31a4cc8f6f65be67bd7af4a95351464931
Devki1 <gdev3195@gmail.com>
2020-04-11 16:49:35 +0530

    ADDED:Test case check given length

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class QuantityMeasurementTest {
     QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+
     @Test
     public void givenLengthsInFeet_ShouldReturnTrue() {
         Feet feet1 = new Feet(0.0);
@@ -15,6 +16,7 @@ public class QuantityMeasurementTest {
         Assert.assertEquals("equal", result1);
         Assert.assertEquals("not equal", result2);
     }
+
     @Test
     public void givenTwoFeetObjects_IfOneOfThemIsNull_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
@@ -22,12 +24,14 @@ public class QuantityMeasurementTest {
         String result1 = quantityMeasurement.compare(feet1, feet2);
         Assert.assertEquals("not equal", result1);
     }
+
     @Test
     public void givenTwoFeetObjects_IfObjectsAreSame_ShouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
         String result1 = quantityMeasurement.compare(feet1, feet1);
         Assert.assertEquals("equal", result1);
     }
+
     @Test
     public void givenTwoObjects_IfTypeIsSame_ShouldReturnResult() {
         Feet feet1 = new Feet(0.0);
@@ -35,12 +39,25 @@ public class QuantityMeasurementTest {
         String result1 = quantityMeasurement.compare(feet1, feet2);
         Assert.assertEquals("equal", result1);
     }
+
     @Test
     public void givenTwoObjects_IfValuesAreSame_ShouldReturnEqual() {
         Feet feet1 = new Feet(12.0);
         Feet feet2 = new Feet(12.0);
         String result1 = quantityMeasurement.compare(feet1, feet2);
         Assert.assertEquals("equal", result1);
+    }
+
+    @Test
+    public void givenLengthsInInch_ShouldReturnTrue() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Inch inch3 = new Inch(1.0);
+        boolean result1 = quantityMeasurement.compareInch(inch1, inch2);
+        boolean result2 = quantityMeasurement.compareInch(inch1, inch3);
+        Assert.assertEquals(true, result1);
+        Assert.assertEquals(false, result2);
+
     }
 }
 
