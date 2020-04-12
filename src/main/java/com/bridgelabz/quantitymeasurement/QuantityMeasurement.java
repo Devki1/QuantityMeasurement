b11c5e73884d5f5e0caec55824e068f68c9c3127
Devki1 <gdev3195@gmail.com>
2020-04-12 19:31:51 +0530

    ADDED:Create a method to add two value

--- src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
@@ -1,12 +1,13 @@
 package com.bridgelabz.quantitymeasurement;
 
 public class QuantityMeasurement {
-    public boolean compare(Distance value1, Distance value2)
-    {
-        if(value1.equals(value2))
-            return  true;
+    public boolean compare(Distance value1, Distance value2) {
+        if (value1.equals(value2))
+            return true;
         return false;
     }
+
+    public Double addTwoLengths(Distance value1, Distance value2) {
+        return value1.distance + value2.distance;
+    }
 }
-
-
