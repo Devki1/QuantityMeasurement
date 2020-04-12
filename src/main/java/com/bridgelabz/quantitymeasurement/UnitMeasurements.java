2905717caf10397e4a32f5743ed52a26b86fbaf5
Devki1 <gdev3195@gmail.com>
2020-04-12 20:10:46 +0530

    REFACTORED:Change the value of gallon liter and milliliter

--- src/main/java/com/bridgelabz/quantitymeasurement/UnitMeasurements.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/UnitMeasurements.java
@@ -2,7 +2,8 @@ package com.bridgelabz.quantitymeasurement;
 
 public enum UnitMeasurements {
     FEET(12),INCH(1),
-    YARD(36),CENTIMETER(0.4);
+    YARD(36),CENTIMETER(0.4),
+    GALLON(3.78), LITRES(1), MILLILITERS(0.001);
 
     Double baseValue;
     UnitMeasurements(double baseValue) {
